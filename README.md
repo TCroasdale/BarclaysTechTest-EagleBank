32a0cf2fca24456cc4adc5faaf5c29a70c278700
Tom Croasdale <tecroasdale@gmail.com>
2025-09-05 18:02:27 +0100

    Include interface auto generated from openapi spec

---
+++ README.md
@@ -0,0 +1,5 @@
+# To install dependancies & generate models
+``` ./mvnw clean install ```
+
+# To run
+``` ./mvnw spring-boot:run ```
--- src/main/java/com/techtest/eaglebank/eaglebank/EaglebankApplication.java
+++ src/main/java/com/techtest/eaglebank/eaglebank/EaglebankApplication.java
@@ -2,8 +2,12 @@ package com.techtest.eaglebank.eaglebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.baeldung.openapi.api.V1ApiController;
 
 @SpringBootApplication
+@Import({ V1ApiController.class })
 public class EaglebankApplication {
 
 	public static void main(String[] args) {
